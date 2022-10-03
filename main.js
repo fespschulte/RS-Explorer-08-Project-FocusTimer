59a62a46e05e3e446c566c707c4588dce786c414
Fernando Pinto <fschultepinto@gmail.com>
2022-10-03 15:09:33 -0300

    Evento para mudar de play para pause baseado no clique

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // Arquivo principal para script do temporizador
+
+let play = document.querySelector('.play')
+let pause = document.querySelector('.pause')
+
+play.addEventListener('click', function() {
+  play.classList.add('hide')
+
+  pause.classList.remove('hide')
+
+})
+
+pause.addEventListener('click', function() {
+  play.classList.remove('hide')
+
+  pause.classList.add('hide')
+
+})
+
+
