870919e0d55f921e5ef9771dcbcd6598375c4b95
Fernando Pinto <fschultepinto@gmail.com>
2022-10-03 16:40:42 -0300

    Lógica do botão de stop

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
 // Arquivo principal para script do temporizador
 
-let buttonPlay = document.querySelector('.play')
-let buttonPause = document.querySelector('.pause')
-let buttonStop = document.querySelector('.stop')
-let buttonSet = document.querySelector('.set')
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
 
 
 
-play.addEventListener('click', function() {
+buttonPlay.addEventListener('click', function() {
   buttonPlay.classList.add('hide')
 
   buttonPause.classList.remove('hide')
+  buttonSet.classList.add('hide')
+  buttonStop.classList.remove('hide')
 
 })
 
-pause.addEventListener('click', function() {
+buttonPause.addEventListener('click', function() {
   buttonPlay.classList.remove('hide')
 
   buttonPause.classList.add('hide')
+
+
+})
+
+buttonStop.addEventListener('click', function() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide')
 
 })
 
