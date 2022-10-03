695b88a578e318f509d656c47df89df939f96aad
Fernando Pinto <fschultepinto@gmail.com>
2022-10-03 19:38:54 -0300

    Implementando funções para declarar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // Arquivo principal para script do temporizador
 
+// import { resetControls } from "./utils"
+
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
 const buttonStop = document.querySelector('.stop')
@@ -10,21 +12,39 @@ let minutes
 const minutesDisplay = document.querySelector(".minutes")
 const secondsDisplay = document.querySelector(".seconds")
 
+function resetControls() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide')
+}
+
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+
 function countDown () {
   setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent)
     let minutes = Number(minutesDisplay.textContent)
+
+    secondsDisplay.textContent = "00"
+    
+    if (minutes <= 0) {
+      resetControls()
+      return
+    }
 
     if( seconds <= 0 ) {
       seconds = 60
 
       minutesDisplay.textContent = minutes - 1
     }
+    
     secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+    minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
 
-    if (minutes <= 0) {
-      return
-    }
 
     countDown()
   }, 1000)
@@ -51,10 +71,7 @@ buttonPause.addEventListener('click', function() {
 })
 
 buttonStop.addEventListener('click', function() {
-  buttonPlay.classList.remove('hide')
-  buttonPause.classList.add('hide')
-  buttonSet.classList.remove('hide')
-  buttonStop.classList.add('hide')
+  resetControls()
 
 })
 
@@ -70,7 +87,7 @@ buttonSoundOff.addEventListener('click', function () {
 
 buttonSet.addEventListener('click', function() {
   minutes = prompt('Quantos minutos?')
-  minutesDisplay.textContent = minutes
+  updateTimerDisplay(minutes, 0)
 })
 
 
