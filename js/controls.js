48629fca8ece950042a75b6fdb077d54ab085dd4
Fernando Pinto <fschultepinto@gmail.com>
2022-10-05 14:22:18 -0300

    Modularizando, implementando factory e injetando dependências

---
+++ js/controls.js
@@ -0,0 +1,10 @@
+export function Controls({
+
+}) {
+  function resetControls() {
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+  }
+}
