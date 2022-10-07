b956ac955701119688ab579157e7a3cabff9c8b7
Fernando Pinto <fschultepinto@gmail.com>
2022-10-07 18:40:39 -0300

    Ajuste de sintaxe

--- js/timer.js
+++ js/timer.js
@@ -8,6 +8,7 @@ export default function Timer({
   let minutes = Number(minutesDisplay.textContent)
 
   function updateDisplay(newMinutes, seconds) {
+  
     newMinutes = newMinutes === undefined ? minutes : newMinutes
     seconds = seconds === undefined ? 0 : seconds
     minutesDisplay.textContent = String(newMinutes).padStart(2, "0")
@@ -18,31 +19,30 @@ export default function Timer({
     updateDisplay(minutes, 0)
     clearTimeout(timerTimeOut)
   }
-  
-  
-  function countDown() {
-  
-    timerTimeOut = setTimeout(function () {
+
+  function countDown(){
+    timerTimeOut = setTimeout(function() {
       let seconds = Number(secondsDisplay.textContent)
       let minutes = Number(minutesDisplay.textContent)
-  
+      let isFinished = minutes <= 0 && seconds <= 0
+
       updateDisplay(minutes, 0)
-      
-      if (minutes <= 0 && seconds <= 0) {
+
+      if (isFinished) {
         resetControls()
+        updateDisplay()
         return
       }
-  
+
       if( seconds <= 0 ) {
         seconds = 60
         --minutes
       }
-  
+
       updateDisplay(minutes, String(seconds - 1))
-  
+
       countDown()
     }, 1000)
-  
   }
 
   function updateMinutes(newMinutes) {
@@ -51,7 +51,6 @@ export default function Timer({
 
   function hold() {
     clearTimeout(timerTimeOut)
-
   }
 
   return {
