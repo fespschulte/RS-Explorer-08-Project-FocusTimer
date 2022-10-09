0a77453dbca7c88c82bec483f65bd0e5eaf6e294
Fernando Pinto <fschultepinto@gmail.com>
2022-10-09 15:52:56 -0300

    Arquivos sons

--- js/sounds.js
+++ js/sounds.js
@@ -1,3 +1,5 @@
+// Arquivo para sons
+
 export default function() {
   const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
   const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
